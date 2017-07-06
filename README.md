dbab4a3709fd167e57eaf0ed47fa5729528a6c26
emersion <contact@emersion.fr>
2017-07-06 18:57:58 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # tooru
-Archive and sync content
+
+Archive and sync content you want to see twice.
+
+Inspired from one of [@SirCmpwn](https://github.com/SirCmpwn)'s articles : http://sircmpwn.github.io/2017/06/19/Archive-it-or-miss-it.html
+
+## Usage
+
+Create a file named `sources` with one link per line and run `tooru`:
+
+```bash
+mkdir ~/archives
+cd ~/archives
+echo "https://drewdevault.com" >> sources
+tooru
+```
+
+You can setup a cronjob to sync your archive periodically:
+
+```
+0 0 * * 0 cd ~/archives && tooru
+```
+
+## Supported content
+
+* Everything that `youtube-dl` supports
+* Falls back to `wget`
+
+## License
+
+MIT
---
+++ tooru.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+sources_file=sources
+if [ "$#" -eq 1 ] ; then
+	sources_file="$1"
+fi
+
+url_host() {
+	echo "$1" | cut -d'/' -f3
+}
+
+ytdl_check() {
+	env url="$1" python << EOF
+import os
+from youtube_dl.extractor import get_info_extractor, gen_extractor_classes, _LAZY_LOADER
+
+url = os.getenv('url')
+for ie in gen_extractor_classes():
+	if ie.ie_key() != "Generic" and ie.suitable(url):
+		exit(0)
+exit(1)
+EOF
+}
+
+while read src
+do
+	echo "Archiving $src"
+	if ytdl_check "$src"; then
+		host=$(url_host "$src")
+		youtube-dl -w -o "$host/%(playlist)s/%(id)s.%(ext)s" --add-metadata "$src"
+	else
+		wget -m "$src"
+	fi
+	echo ""
+done < "$sources_file"
